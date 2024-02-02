1d33a6a69aa50ea9f9a3f8dd0ec07438b1d13166
Justscoundrel <justscoundrel@yandex.ru>
2024-02-02 15:46:34 +0300

    All packages

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="odatapython",
-    version="0.0.1",
+    version="0.0.2",
     author="Andrey Galkin",
     author_email="justscoundrel@yandex.ru",
     description="API for odata 1C",
